2d5496cad6ad1c27510bdcacf0a572c848bb302d
Eric Fetterolf <47679372+BrokeChewbacca@users.noreply.github.com>
2019-03-09 20:24:40 -0500

    Update reboot.py
    Added 'is_dst' function to check if it is daylight-savings time or standard time. Also added environment variables for each maintenance window and time zone updates.

--- reboot.py
+++ reboot.py
@@ -1,42 +1,51 @@
 from __future__ import print_function
 import boto3
 from botocore.exceptions import ClientError
-from datetime import datetime
+import pytz
+from datetime import datetime, timedelta
+import os
 
 print('Loading function')
 
-def lambda_handler(event, context):
-    message = event['Records'][0]['Sns']['Message']
-    print("From SNS: " + message)
-    
-    # The four lines below get the instance id that triggered the alert by splitting
-    # the message string until all that is left is the Instance ID
-    message = message.split('"value":"', 1)
-    message = message[1].split('",', 1)
-    instanceID = message[0]
+def is_dst(zonename):
+	tz = pytz.timezone(zonename)
+	now = pytz.utc.localize(datetime.utcnow())
+	return now.astimezone(tz).dst() != timedelta(0)
 
-    # Gets the current date/time, since this is run in AWS this time is in UTC
-    currentDT = datetime.now()
-    # Strips the date portion off, leaving it in HH:MM:SS format
-    currentDT = currentDT.strftime('%H:%M:%S')
-    
-    # Reminder, the time below is adjusted for UTC
-    # Below it is set to reboot if the current time is between 03:00:00 UTC
-    # and 11:00:00 UTC
-    if currentDT > '03:00:00' and currentDT < '11:00:00':
-        ec2 = boto3.client('ec2')
-        
-        try:
-            ec2.reboot_instances(InstanceIds=[instanceID], DryRun=True)
-        except ClientError as e:
-            if 'DryRunOperation' not in str(e):
-                print("You don't have permission to reboot instances.")
-                raise
-        
-        try:
-            response = ec2.reboot_instances(InstanceIds=[instanceID], DryRun=False)
-            print('Success', response)
-        except ClientError as e:
-            print('Error', e)
-            
-    return message
+def lambda_handler(event, context):
+	message = event['Records'][0]['Sns']['Message']
+	print('From SNS: ' + message)
+
+	holderStr = message
+	holderStr = holderStr.split('"value":"', 1)
+	holderStr = holderStr[1].split('",', 1)
+	instanceID = holderStr[0]
+
+	currentDT = datetime.utcnow()
+	currentDT = currentDT.strftime('%H:%M:%S')
+
+	tz = os.environ['TimeZone']
+	if is_dst(tz):
+		maintWindowStartTime = os.environ['MaintWindowStart_DST']
+		maintWindowEndTime = os.environ['MaintWindowEnd_DST']
+	else:
+		maintWindowStartTime = os.environ['MaintWindowStart_ST']
+		maintWindowEndTime = os.environ['MaintWindowEnd_ST']
+
+	if currentDT > maintWindowStartTime and currentDT < maintWindowEndTime:
+		ec2 = boto3.client('ec2')
+
+		try:
+			ec2.reboot_instances(InstanceIds=[instanceID], DryRun=True)
+		except ClientError as e:
+			if 'DryRunOperation' not in str(e):
+				print("You do not have permission to reboot instances.")
+				raise
+
+		try:
+			response = ec2.reboot_instances(InstanceIds=[instanceID], DryRun=False)
+			print('Reboot initiated for: ' + instanceID, response)
+		except ClientError as e:
+			print('Error', e)
+
+	return message
